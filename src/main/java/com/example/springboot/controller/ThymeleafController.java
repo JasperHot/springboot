422fc71ede2d6555cb4712f8ae7f625208dc5199
jasper <realshuqian@yahoo.com>
2018-04-15 14:08:46 +0800

    add amap js api

--- src/main/java/com/example/springboot/controller/ThymeleafController.java
+++ src/main/java/com/example/springboot/controller/ThymeleafController.java
@@ -2,7 +2,10 @@ package com.example.springboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -10,15 +13,27 @@ import java.util.Locale;
 
 @Controller
 public class ThymeleafController {
-    @RequestMapping("/hi")
-    public String hello(Locale locale, Model model) {
-        model.addAttribute("greeting", "Hello!");
+    @RequestMapping(value = "/hi/{params}",method = RequestMethod.GET)
+    public String hello1(@PathVariable("params") String params, Locale locale, Model model) {
+        model.addAttribute("greeting", params);
 
         Date date = new Date();
         DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
         String formattedDate = dateFormat.format(date);
         model.addAttribute("currentTime", formattedDate);
 
-        return "hello";
+        return params;
+    }
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String hello2(@RequestParam(value = "params", defaultValue = "hello") String params, Locale locale, Model model) {
+        model.addAttribute("greeting", params);
+
+        Date date = new Date();
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+        String formattedDate = dateFormat.format(date);
+        model.addAttribute("currentTime", formattedDate);
+
+        return params;
     }
 }
